2ad15b53fb67a651a5da12832deaeb0534f2c46e
Rafael Fernández López <ereslibre@ereslibre.es>
2010-02-24 13:03:39 +0100

    Quita espacio en blanco

--- practicas/src/agsimple/Cromosoma.java
+++ practicas/src/agsimple/Cromosoma.java
@@ -1,7 +1,7 @@
 /**
  * Copyright 2010 Rafael Fernández López <ereslibre@ereslibre.es>
  * Copyright 2010 Ángel Valero Picazo <valeropc@gmail.com>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
