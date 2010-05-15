0837456055fc85163a3252f7ab18e43854e2089e
Ángel Valero Picazo <valeropc@gmail.com>
2010-05-15 12:53:54 +0200

    Añade toString a Arbol

--- G01P3/src/practica3/Arbol.java
+++ G01P3/src/practica3/Arbol.java
@@ -19,95 +19,25 @@ import java.util.ArrayList;
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-///*Estructura de los datos de entrenamiento*/
-//struct TDatos {
-//    char planeta[LONG_MAX_NOM];
-//    float A;    /* Distancia media al sol */
-//    float P;    /* Periodo orbital */
-//};
-//
-///*Estructura de las funciones*/
-//struct TFuncion {
-//   int aridad;
-//   String nombreFun;
-//};
-//
-///* Clase que implementa los árboles de expresiones */
-//class TArbol {
-//
-//  /* Atributos privados de la clase */
-//  private:
-//    String nombre;
-//    TArbol* hd;
-//    TArbol* hi;
-//    TArbol* padre;
-//    int profundidad;
-//    int numNodos;
-//    int posSimbolo;
-//    bool hoja, raiz;
-//    bool esHi;  /*Indica si el nodo es hijo izquierdo de su padre o no*/
-//
-//  public:
-//
-//    /* Constructoras de la clase */
-//    TArbol() {profundidad = 0;};
-//    TArbol(TArbol* arbol, TArbol* pater);
-//    TArbol(vector<TFuncion> * cjtoFuns, vector<String> *  cjtoTerms,
-//      int hmax, int prof, TArbol* pater, bool esHizq, bool esRaiz);
-//
-//    /* Métodos para acceder a las variables privadas */
-//    String leerNombre() { return nombre; };
-//    int leerPosSimbolo() { return posSimbolo; };
-//    TArbol* leerHd() { return hd; };
-//    TArbol* leerHi() { return hi; };
-//    TArbol* leerPadre() { return padre; };
-//    bool esHoja() { return hoja; };
-//    bool esRaiz() { return raiz; };
-//    bool esHijoIzquierdo() { return esHi; };
-//    int leerProfundidad() { return profundidad;};
-//    int leerNumNodos() { return numNodos;};
-//
-//    /* Métodos para escribir las variables privadas */
-//    void escribirProfundidad(int p) { profundidad=p;};
-//    void escribirNumNodos(int n) { numNodos=n;};
-//    void escribirHd(TArbol* nodo);
-//    void escribirHi(TArbol* nodo);
-//    void escribirPadre(TArbol* pater) { padre = pater; };
-//    void escribirRaiz(bool esRaiz) { raiz = esRaiz; };
-//
-//    /* Métodos propios de la clase */
-//    String mostrar();
-//    float evaluar(float a);
-//    int altura();
-//    void actualizar(int prof);
-//    TArbol* nodoAleatorio();
-//    TArbol* buscarNodo(int n);
-//    void mutaNodo( vector<TFuncion>  cjtoFuns, vector<String>  cjtoTerms);
-//
-//};
-//
-//
-//#endif
-
 public class Arbol {
+
 	private Arbol      m_hi = null;
 	private Arbol      m_hc = null;
 	private Arbol      m_hd = null;
 	private Arbol      m_padre = null;
-	private int        m_profundidad = 0;
-	private int        m_funcion = -1;
-	private int        m_termino = -1;
+	private Funcion    m_funcion = null;
+	private Termino    m_termino = null;
 
 	public Arbol(ArrayList<Termino> terminos, Arbol padre) {
 		m_padre = padre;
-		m_termino = (int) Math.round(Math.random() * (terminos.size() - 1));
+		m_termino = terminos.get((int) Math.round(Math.random() * (terminos.size() - 1)));
 	}
 
 	public Arbol(ArrayList<Funcion> funciones, ArrayList<Termino> terminos, Arbol padre, int alturaMaxima, int altura) {
 		m_padre = padre;
-		m_funcion = (int) Math.round(Math.random() * Funcion.TotalFunciones);
+		m_funcion = funciones.get((int) Math.round(Math.random() * Funcion.TotalFunciones));
 		if (altura == alturaMaxima) {
-			switch (funciones.get(m_funcion).aridad()) {
+			switch (m_funcion.aridad()) {
 				case 2:
 					m_hd = new Arbol(Funcion.terminosControl(), this); // fall-through
 				case 1:				
@@ -123,13 +53,13 @@ public class Arbol {
 			}
 			return;
 		}
-		switch (funciones.get(m_funcion).aridad()) {
+		switch (m_funcion.aridad()) {
 			case 2:
 				if (Math.random() < 0.5) {
 					m_hd = new Arbol(Funcion.terminosControl(), this);
 				} else {
 					m_hd = new Arbol(funciones, terminos, this, alturaMaxima, altura + 1);
-				}
+				} // fall-through
 			case 1:
 				if (Math.random() < 0.5) {
 					m_hi = new Arbol(Funcion.terminosControl(), this);
@@ -199,5 +129,29 @@ public class Arbol {
 
 	public Arbol padre() {
 		return m_padre;
+	}
+
+	public String toString() {
+		if (m_funcion == null) {
+			return m_termino.nombre();
+		}
+		String res = new String();
+		switch (m_funcion.funcion()) {
+			case Funcion.Not:
+				res += "(NOT " + m_hi.toString() + ")";
+				break;
+			case Funcion.And:
+				res += "(AND " + m_hi.toString() + " " + m_hd.toString() + ")";
+				break;
+			case Funcion.Or:
+				res += "(OR " + m_hi.toString() + " " + m_hd.toString() + ")";
+				break;
+			case Funcion.If:
+				res += "(IF " + m_hi.toString() + " " + m_hc.toString() + " " + m_hd.toString() + ")";
+				break;
+			default:
+				break;
+		}
+		return res;
 	}
 }
